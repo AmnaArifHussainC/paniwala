0672cdb961c18d35b2f04f363a626b5616700499
AmnaArifHussainC <amnaarifkeepup@gmail.com>
2025-02-03 16:30:17 +0500

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:paniwala/splash_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: BoardingScreen(),
     );
   }
 }
