896bd8445927c22904b8725a37991c1d742992aa
AmnaArifHussainC <amnaarifkeepup@gmail.com>
2025-02-04 11:33:07 +0500

    Merge remote-tracking branch 'origin/master'

---
+++ lib/const/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBlue = Color(0xFFADD8E6);
+  static const Color skyBlue = Color(0xFF87CEEB);
+  static const Color dodgerBlue = Color(0xFF1E90FF);
+  static const Color deepSkyBlue = Color(0xFF00BFFF);
+  static const Color navyBlue = Color(0xFF000080);
+  static const Color midnightBlue = Color(0xFF191970);
+}
