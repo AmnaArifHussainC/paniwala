27789ac818b95ba9adea433933e710a62312099f
Ghulam Fatima <gh2468455@gmail.com>
2025-02-04 10:44:41 +0500

    commit by fatima

---
+++ lib/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:paniwala/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => BoardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/images/icon.png', width: 200), // Your logo
+      ),
+    );
+  }
+}
