fe1e17ab91b1ed7b121983b2c63805e4d68a727a
AmnaArifHussainC <amnaarifkeepup@gmail.com>
2025-02-07 13:41:08 +0500

    Merge remote-tracking branch 'origin/master'

---
+++ lib/view/supplier_screen/supplier_dash_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SupplierDashScreen extends StatelessWidget {
+  const SupplierDashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
