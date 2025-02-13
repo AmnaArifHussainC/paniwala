ce5df78ad4bd327442a012f4406258c53f573b14
Ghulam Fatima <gh2468455@gmail.com>
2025-02-13 14:30:28 +0500

    commit by Fatima

--- lib/view/startup/splahScreen.dart
+++ lib/view/startup/splahScreen.dart
@@ -6,7 +6,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'onboarding_screen.dart';
 
-
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
